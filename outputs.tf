54d739438b2c6a544858bbdaea46741af03e40d8
vivek kovi <vivekkovi93@gmail.com>
2026-01-25 17:56:40 -0600

    fix:

--- outputs.tf
+++ outputs.tf
@@ -6,7 +6,3 @@ output "instance_id" {
   description = "ID of the EC2 instance"
   value       = module.terraform-aws-ec2.instance_id
 }
-output "security_group_id" {
-  description = "ID of the security group allowing SSH"
-  value       = module.terraform-aws-ec2.security_group_id
-}
