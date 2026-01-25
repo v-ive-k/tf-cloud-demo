27ce417697d10166fb2e7ece6412bd23e8dfafb5
vivek kovi <vivekkovi93@gmail.com>
2026-01-25 16:54:06 -0600

    made changes to variable.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,8 @@
-# variable "region" {
-#   description = "AWS region"
-#   type        = string
-#   default     = "us-east-1"
-# }
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
 
 variable "instance_type" {
   description = "EC2 instance type"
