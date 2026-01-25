a26f067399f7956085998135d09da6dfe3e9f0ee
vivek kovi <vivekkovi93@gmail.com>
2026-01-25 17:46:45 -0600

    tfc module used

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
-module "ec2" {
-  source = "git::https://github.com/v-ive-k/terraform-aws-ec2.git?ref=v1.0.0"
-
+module "terraform-aws-ec2" {
+  source  = "app.terraform.io/Vivek-Infra/terraform-aws-ec2/aws"
+  version = "1.0.0"
+  
   ami_id         = "ami-0c02fb55956c7d316"
   instance_type  = "t2.micro"
   instance_name  = "vivek-module-ec2"
