ba174ed6753af6fab749474ac770db37dd2dc9db
vivek kovi <vivekkovi93@gmail.com>
2026-01-26 14:34:42 -0600

    import-practise

--- main.tf
+++ main.tf
@@ -12,3 +12,18 @@ module "terraform-aws-ec2" {
     ManagedBy   = "terraform"
   }
 }
+
+module "terraform-aws-ec2-imported" {
+  source  = "app.terraform.io/Vivek-Infra/terraform-aws-ec2/aws"
+  version = "1.0.0"
+  
+  ami_id         = "ami-0532be01f26a3de55"
+  instance_type  = "t2.micro"
+  instance_name  = "imported-ec2"
+
+  tags = {
+    ManagedBy = "terraform"
+    Purpose   = "import-demo"
+  }
+}
+
--- provider.tf
+++ provider.tf
@@ -10,3 +10,13 @@ terraform {
 provider "aws" {
   region = var.region
 }
+
+terraform {
+  cloud {
+    organization = "Vivek-Infra"
+
+    workspaces {
+      name = "tf-cloud-demo"
+    }
+  }
+}
