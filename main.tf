8eb7f45936a8805c6700c69e4ffbf825c0aeb275
v-ive-k <vivekkovi93@gmail.com>
2026-02-01 17:09:29 -0600

    Update EC2 module version to 1.0.2

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ module "terraform-aws-ec2" {
 
 module "terraform-aws-ec2-imported" {
   source  = "app.terraform.io/Vivek-Infra/terraform-aws-ec2/aws"
-  version = "1.0.1"
+  version = "1.0.2"
   
   ami_id         = "ami-0532be01f26a3de55"
   instance_type  = "t2.micro"
